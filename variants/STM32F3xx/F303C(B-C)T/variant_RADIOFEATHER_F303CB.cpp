119f43167489a27f146433933432590de49095a7
cypherfoundry <cypherfoundry@protonmail.com>
2023-06-03 22:48:54 +1000

    fixing HSE clock

--- variants/STM32F3xx/F303C(B-C)T/variant_RADIOFEATHER_F303CB.cpp
+++ variants/STM32F3xx/F303C(B-C)T/variant_RADIOFEATHER_F303CB.cpp
@@ -82,9 +82,9 @@ extern "C"
      */
     void SystemClock_Config(void)
     {
-        RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-        RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-        RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+        RCC_OscInitTypeDef RCC_OscInitStruct = {};
+        RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+        RCC_PeriphCLKInitTypeDef PeriphClkInit = {};
 
         /** Configure LSE Drive Capability
          */
@@ -94,11 +94,11 @@ extern "C"
         /** Initializes the RCC Oscillators according to the specified parameters
          * in the RCC_OscInitTypeDef structure.
          */
-        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
+        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
         RCC_OscInitStruct.HSEState = RCC_HSE_ON;
         RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
-        RCC_OscInitStruct.LSEState = RCC_LSE_ON;
         RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+        RCC_OscInitStruct.LSIState = RCC_LSI_ON;
         RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
         RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
         RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
@@ -109,18 +109,19 @@ extern "C"
 
         /** Initializes the CPU, AHB and APB buses clocks
          */
-        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                    |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
         RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
         RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
         if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
         {
             Error_Handler();
         }
-        PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_RTC;
-        PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+        PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_RTC;
+        PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
         PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
         if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
         {
